ab3f39029037b3faeb42aa0b7e3385f22272c656
Bozhovskiy <roman.bozhovskyi.pm.2021@lpnu.ua>
2022-11-09 21:18:55 +0200

    Webpack setting

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     entry: { App: path.resolve(__dirname, "./src/index.js") ,
         },
     output: {
-        path: path.resolve(__dirname, "./dist"),
+        path: path.resolve(__dirname, "./build"),
         filename:  '[name].js',
     },
     optimization: {
