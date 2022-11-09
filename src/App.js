8815309faa46991f8ed98ad7034e8c42df02fcae
Bozhovskiy <roman.bozhovskyi.pm.2021@lpnu.ua>
2022-11-09 19:38:35 +0200

    Webpack setting

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App';
+import './scss/divide'
+function App() {
+
+  return (
+    <div className="App">
+        <div className="divLeft" ></div>
+        <div className="divRight"></div>
+    </div>
+  );
+}
+
+export default App;
